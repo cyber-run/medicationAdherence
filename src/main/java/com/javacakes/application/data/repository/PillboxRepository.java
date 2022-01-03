334732550259f91fd0887c031cba671cd863b92e
June(석준) <june3760@gmail.com>
2022-01-03 21:54:18 +0000

    data / repository update -Patient -Medication -Pillbox -Adherence

---
+++ src/main/java/com/javacakes/application/data/repository/PillboxRepository.java
@@ -0,0 +1,8 @@
+package com.javacakes.application.data.repository;
+
+import com.javacakes.application.data.entity.Pillbox;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PillboxRepository extends JpaRepository<Pillbox, Integer> {
+
+}

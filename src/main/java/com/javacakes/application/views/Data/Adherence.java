6a2cf14217398daeb5fe21cd8d0e0fc8a1a99990
jemhs <jemimahsu@yahoo.co.uk>
2022-01-04 16:45:10 +0000

    Percentage adherence chart

--- src/main/java/com/javacakes/application/views/Data/Adherence.java
+++ src/main/java/com/javacakes/application/views/Data/Adherence.java
@@ -1,7 +1,10 @@
 package com.javacakes.application.views.Data;
 
+import com.javacakes.application.data.service.JavacakeService;
 import com.javacakes.application.views.DefaultLayout;
-import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.charts.Chart;
+import com.vaadin.flow.component.charts.model.*;
+import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -13,9 +16,38 @@ import javax.annotation.security.PermitAll;
 @PageTitle("Adherence")
 public class Adherence extends VerticalLayout {
 
-    H1 heading = new H1("Medication Timing Adherence");
+    H2 heading = new H2("Medication Timing Adherence");
 
-    public Adherence() {
-        add(heading);
+    // New chart
+    Chart chart = new Chart(ChartType.COLUMN);
+    Configuration conf = chart.getConfiguration();
+    XAxis xaxis = new XAxis();
+    YAxis yaxis = new YAxis();
+
+
+    JavacakeService service;
+
+    public Adherence(JavacakeService service) {
+        // X-axis
+        xaxis.setTitle("Pill Type");
+        conf.addxAxis(xaxis);
+        // Y-axis
+        yaxis.setTitle("Percentage Adherence");
+        conf.addyAxis(yaxis);
+
+        // Reference https://vaadin.com/docs/latest/flow/tutorial/navigation-and-layouts
+        this.service = service;
+        DataSeries dataSeries = new DataSeries();
+        service.findAllAdherence().forEach(adherence -> dataSeries.add(
+                new DataSeriesItem(adherence.getMedication().getPillType(),
+                                    adherence.getWeeklyAdherence())));
+        chart.getConfiguration().setSeries(dataSeries);
+
+        conf.getLegend().setEnabled(false);
+
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        // End of reference
+
+        add(heading, chart);
     }
 }

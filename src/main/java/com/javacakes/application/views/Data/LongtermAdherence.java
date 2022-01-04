59404f2a814f973389133bdf13a771e8776c2851
yhc19marco <yhc19@ic.ac.uk>
2022-01-05 07:30:43 +0800

    Long Term adherence chart

--- src/main/java/com/javacakes/application/views/Data/LongtermAdherence.java
+++ src/main/java/com/javacakes/application/views/Data/LongtermAdherence.java
@@ -1,7 +1,9 @@
 package com.javacakes.application.views.Data;
 
+import com.javacakes.application.data.service.JavacakeService;
 import com.javacakes.application.views.DefaultLayout;
-import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.charts.Chart;
+import com.vaadin.flow.component.charts.model.*;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
@@ -16,7 +18,36 @@ public class LongtermAdherence extends VerticalLayout {
 
     H2 heading = new H2("Long-term Medication Timing Adherence");
 
-    public LongtermAdherence() {
-        add(heading);
+    // New chart
+    Chart chart = new Chart(ChartType.COLUMN);
+    Configuration conf = chart.getConfiguration();
+    XAxis xaxis = new XAxis();
+    YAxis yaxis = new YAxis();
+
+
+    JavacakeService service;
+
+    public LongtermAdherence(JavacakeService service) {
+        // X-axis
+        xaxis.setTitle("Pill Type");
+        conf.addxAxis(xaxis);
+        // Y-axis
+        yaxis.setTitle("Long Term Percentage Adherence");
+        conf.addyAxis(yaxis);
+
+        // Reference https://vaadin.com/docs/latest/flow/tutorial/navigation-and-layouts
+        this.service = service;
+        DataSeries dataSeries = new DataSeries();
+        service.findAllAdherence().forEach(adherence -> dataSeries.add(
+                new DataSeriesItem(adherence.getMedication().getPillType(),
+                        adherence.getLongtermAdherence())));
+        chart.getConfiguration().setSeries(dataSeries);
+
+        conf.getLegend().setEnabled(false);
+
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        // End of reference
+
+        add(heading, chart);
     }
 }

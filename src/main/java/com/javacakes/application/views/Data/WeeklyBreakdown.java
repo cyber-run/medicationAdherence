b1b733587314f882a7d3ba6511fb604894ae1db7
yhc19marco <yhc19@ic.ac.uk>
2022-01-09 22:37:43 +0800

    Weekly breakdown chart

--- src/main/java/com/javacakes/application/views/Data/WeeklyBreakdown.java
+++ src/main/java/com/javacakes/application/views/Data/WeeklyBreakdown.java
@@ -1,5 +1,7 @@
 package com.javacakes.application.views.Data;
-
+import com.javacakes.application.data.entity.Medication;
+import com.vaadin.flow.component.gridpro.GridPro;
+import com.javacakes.application.data.service.JavacakeService;
 import com.javacakes.application.views.DefaultLayout;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -7,15 +9,43 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 import javax.annotation.security.PermitAll;
+import java.util.List;
 
 @PermitAll
 @Route(value = "weeklybreakdown", layout = DefaultLayout.class)
 @PageTitle("WeeklyBreakdown")
 public class WeeklyBreakdown extends VerticalLayout {
+    GridPro<Medication> grid = new GridPro<>(Medication.class);
 
+    //declare service
+    JavacakeService service;
+    List<Medication> pillTypes; // Lazy loading because direct methods were causing errors
+    //declare heading
     H1 heading = new H1("Weekly Breakdown");
 
-    public WeeklyBreakdown() {
-        add(heading);
+    //Method for home page & pass in service
+    public WeeklyBreakdown(JavacakeService service) {
+
+        this.service = service;
+        pillTypes =  service.findAllMedication();
+
+        setSizeFull();
+        setGrid();
+
+
+        add(
+                heading,
+                grid
+        );
+    }
+
+    private void setGrid() {
+        grid.setSizeFull();
+        grid.setColumns();
+        grid.addColumn(Medication::getPillType).setHeader("Medication name");
+        grid.addColumn(Medication::getPillSchedule).setHeader("Last Seven Days");
+        grid.getColumns().forEach(adherenceColumn -> adherenceColumn.setAutoWidth(true));
+        grid.setItems(service.findAllMedication());
     }
 }
+
